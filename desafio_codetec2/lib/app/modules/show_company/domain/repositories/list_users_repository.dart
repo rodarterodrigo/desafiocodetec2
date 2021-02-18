807303a02a6e2cb28a89704f9292d771eb37a630
Rodrigo Morais Rodarte <rodarte.rodrigo@hotmail.com>
2021-02-18 13:01:44 -0300

    retirada de código inútil

--- desafio_codetec2/lib/app/modules/show_company/domain/repositories/list_users_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:desafio_codetec2/app/modules/show_company/domain/entities/user.dart';
-import 'package:desafio_codetec2/app/modules/show_company/errors/company_errors.dart';
-
-abstract class IListUsersRepository{
-  Future<Either<FailureShowCompany, List<User>>> listUsers();
-}
